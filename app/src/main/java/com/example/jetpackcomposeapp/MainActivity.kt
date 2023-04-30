e3fff8211a8b1d3b81db6c94e3180cbefc41b7b4
deepak252 <trojanhorsedc@gmail.com>
2023-05-01 01:20:21 +0530

    Bottom Navigation using Voyager

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -3,20 +3,66 @@ package com.example.jetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import com.example.jetpackcomposeapp.components.tabs.FavoritesTab
+import com.example.jetpackcomposeapp.components.tabs.HomeTab
+import com.example.jetpackcomposeapp.components.tabs.ProfileTab
 import com.example.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            JetpackComposeAppTheme() {
+                Surface {
+                    Content()
+                }
+            }
         }
+    }
+
+    @Composable
+    fun Content(){
+        TabNavigator(tab = HomeTab){
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation() {
+                        TabNavigationItem(tab = HomeTab)
+                        TabNavigationItem(tab = FavoritesTab)
+                        TabNavigationItem(tab = ProfileTab)
+                    }
+                }
+            ) {paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues))  {
+                    CurrentTab()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun RowScope.TabNavigationItem(tab : Tab){
+        val tabNavigator = LocalTabNavigator.current
+        BottomNavigationItem(
+            selected = tabNavigator.current==tab,
+            onClick = {
+                      tabNavigator.current=tab
+                      },
+            icon = { Icon(painter = tab.options.icon!!, contentDescription = tab.options.title) }
+
+        )
     }
 }
---
+++ app/src/main/java/com/example/jetpackcomposeapp/components/tabs/HomeTab.kt
@@ -0,0 +1,32 @@
+package com.example.jetpackcomposeapp.components.tabs
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.example.jetpackcomposeapp.screens.HomeScreen
+
+object HomeTab : Tab {
+    override val options: TabOptions
+        @Composable
+        get() {
+            val icon = rememberVectorPainter(Icons.Default.Home)
+
+            return remember {
+                TabOptions(
+                    index = 0u,
+                    title = "Home",
+                    icon = icon
+                )
+            }
+        }
+
+    @Composable
+    override fun Content()
+    {
+        HomeScreen()
+    }
+}
